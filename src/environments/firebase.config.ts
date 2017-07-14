70eac940b716d20d08067555b7944ca3ed35cdad
Tom Wilson <tomwon@hotmail.co.uk>
2017-07-14 14:29:30 +0100

    Maths quiz final
    Maths quiz final

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCaNo-cDO4tF1Xh7Czq9rBvbSOgQJ4_9us",
+   authDomain: "damaffsquizz.firebaseapp.com",
+   databaseURL: "https://damaffsquizz.firebaseio.com",
+   storageBucket: "damaffsquizz.appspot.com",
+}
